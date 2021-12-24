d153772aaa36d093e61c419511180e52d402d20f
gonzalm96 <76509293+gonzalm96@users.noreply.github.com>
2021-12-24 04:28:33 -0800

    Update script.js

--- eduardo/script.js
+++ eduardo/script.js
@@ -1,7 +1,7 @@
 $(function(){
     $("#butt").click(function(){
-        $("#vert").animate({height: 0}, 1000, function() {
-            $("#zontal").animate({width: 0}, 500)
+        $("#vert").animate({height: 0}, 200, function() {
+            $("#zontal").animate({width: 0}, 200)
         });
         setTimeout(function(){$("#modal").fadeOut()},1200);
     });
