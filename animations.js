f59d865d65d3da01c194c2a9f932a5ffd7eecaa2
gonzalm96 <mgonzalm96@gmail.com>
2025-01-28 16:49:11 -0800

    Start using this iteration as advertisement

--- animations.js
+++ animations.js
@@ -1,133 +1,53 @@
-$(document).ready(function(){
-    let $home = $("#home");
-    let $about = $('#about');
-    let $project = $('#projects');
-    let $resume = $('#resume');
-    let $contact = $('#contact');
-
-    let $currentlyShowing = $home;
-
-    //slide to home
-    $home.click(function () {
-        $("#" + $currentlyShowing.attr('id') + "-content").css("display", "none");
-        $('#home-content').slideDown({
-            start: function () {
-                $(this).css({
-                    display: "flex"
-                })
-            }
-        });
-        $currentlyShowing = $home;
-    });
-
-    //slide to about
-    $about.click(function(){
-        $("#" + $currentlyShowing.attr('id') + "-content").css("display","none");
-        $('#about-content').slideDown({
-            start: function () {
-                $(this).css({
-                    display: "flex"
-                })
-            }
-        });
-        $currentlyShowing = $about;
-    });
-
-    //slide to projects
-    $project.click(function () {
-        $("#" + $currentlyShowing.attr('id') + "-content").css("display", "none");
-        $('#projects-content').slideDown({
-            start: function () {
-                $(this).css({
-                    display: "flex"
-                })
-            }
-        });
-        $currentlyShowing = $project;
-    });
-
-    //slide to resume
-    $resume.click(function () {
-        $("#" + $currentlyShowing.attr('id') + "-content").css("display", "none");
-        $('#resume-content').slideDown({
-            start: function () {
-                $(this).css({
-                    display: "flex"
-                })
-            }
-        });
-        $currentlyShowing = $resume;
-    });
-
-    //slide to contact
-    $contact.click(function () {
-        $("#" + $currentlyShowing.attr('id') + "-content").css("display", "none");
-        $('#contact-content').slideDown({
-            start: function () {
-                $(this).css({
-                    display: "flex"
-                })
-            }
-        });
-        $currentlyShowing = $contact;
-    });
-});
-
-$("#fuzzy").hover(
-    function () {
-        //fadein second image using jQuery fadeIn 
-        $("#fuzzy-desc").animate({opacity:100},200);
-    }, function(){
-        $("#fuzzy-desc").animate({ opacity: 0 },200);
-    }
-);
-
 $("#flappy").hover(
     function () {
         //fadein second image using jQuery fadeIn 
-        $("#flappy-desc").animate({ opacity: 100 }, 200);
-    }, function () {
-        $("#flappy-desc").animate({ opacity: 0 }, 200);
+        $("#flappy-desc").animate({height: '100%'}, 300);        
+    }, function (){
+        $("#flappy-desc").animate({height: 0}, 300);       
     }
 );
 
-$("#calc").hover(
+$("#recs").hover(
     function () {
         //fadein second image using jQuery fadeIn 
-        $("#calc-desc").animate({ opacity: 100 }, 200);
-    }, function () {
-        $("#calc-desc").animate({ opacity: 0 }, 200);
+        $("#recs-desc").animate({height: '100%'}, 300);        
+    }, function (){
+        $("#recs-desc").animate({height: 0}, 300);       
     }
 );
 
 $("#symply").hover(
     function () {
         //fadein second image using jQuery fadeIn 
-        $("#symply-desc").animate({ opacity: 100 }, 200);
-    }, function () {
-        $("#symply-desc").animate({ opacity: 0 }, 200);
+        $("#symply-desc").animate({height: '100%'}, 300);        
+    }, function (){
+        $("#symply-desc").animate({height: 0}, 300);       
     }
 );
 
-$("#odds").hover(
+$("#xmas").hover(
     function () {
         //fadein second image using jQuery fadeIn 
-        $("#odds-desc").animate({ opacity: 100 }, 200);
-    }, function () {
-        $("#odds-desc").animate({ opacity: 0 }, 200);
+        $("#xmas-desc").animate({height: '100%'}, 300);        
+    }, function (){
+        $("#xmas-desc").animate({height: 0}, 300);       
     }
 );
 
-$("#count").hover(
+$("#figma").hover(
     function () {
         //fadein second image using jQuery fadeIn 
-        $("#count-desc").animate({
-            opacity: 100
-        }, 200);
-    },
+        $("#figma-desc").animate({height: '100%'}, 300);        
+    }, function (){
+        $("#figma-desc").animate({height: 0}, 300);       
+    }
+);
+
+$("#github").hover(
     function () {
-        $("#count-desc").animate({
-            opacity: 0
-        }, 200);
+        //fadein second image using jQuery fadeIn 
+        $("#git-desc").animate({height: '100%'}, 300);        
+    }, function (){
+        $("#git-desc").animate({height: 0}, 300);       
     }
 );
--- script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $("#butt").click(function(){
-        $("#vert").animate({height: 0}, function() {
-            $("#zontal").animate({width: 0})
-        });
-        setTimeout(function(){$("#modal").fadeOut()},1200);
-    });
-});
